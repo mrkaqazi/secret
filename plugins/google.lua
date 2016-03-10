bfe13b4971970860ba586e397e9cd63dfaa39ec3
mahdiam1221 <mahdiam1221@gmail.com>
2016-03-10 08:05:47 +0330

    Update google.lua

--- plugins/google.lua
+++ plugins/google.lua
@@ -34,7 +34,7 @@ return {
   description = "Searches Google and send results",
   usage = "!google [terms]: Searches Google and send results",
   patterns = {
-    "^!google (.*)$",
+    "^گوگل (.*)$",
     "^%.[g|G]oogle (.*)$"
   },
   run = run
