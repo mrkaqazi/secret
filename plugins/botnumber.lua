c75aeb5edb533c6282edc2bbcb244418af68aaf8
mahdiam1221 <mahdiam1221@gmail.com>
2016-03-10 08:00:55 +0330

    Update botnumber.lua

--- plugins/botnumber.lua
+++ plugins/botnumber.lua
@@ -1,7 +1,7 @@
 do
 
 function run(msg, matches)
-send_contact(get_receiver(msg), "+13603353378", "NOD32", "BOT", ok_cb, false)
+send_contact(get_receiver(msg), "+6288976863447", "Viper", "BOT", ok_cb, false)
 end
 
 return {
